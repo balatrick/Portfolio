9b15a5fe3e19bd261ccba779b83e13448f25fb47
balatrick <balatrick>
2023-08-14 14:37:04 +0530

    first commit

--- src/components/TopContent/TopContent.js
+++ src/components/TopContent/TopContent.js
@@ -8,12 +8,12 @@ const TopContent = () => {
         <div className='Topcontent__container'>
         <h1>Mr. Balasubramaniyan Annadurai</h1>
         <h4>Passionate About Creating Functional And Responsive Websites</h4>
-        <a href='www.google.com'><button className='topcontent__downloadbutton'>Download My CV.</button></a>
+        <a href='https://drive.google.com/file/d/1WHYK2l-hn0k1btrm-a3RtS4I9-mh9hqL/view'><button className='topcontent__downloadbutton'>Download My CV.</button></a>
         <Link to="project" smooth="true" duration={500}>
             <button className='topcontent__workbutton'>My work</button>
         </Link>
        </div>      
-    </div>
+    </div> 
   )
 }
 
